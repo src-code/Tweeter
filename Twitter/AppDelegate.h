3e258519c5c6d8927e5f73c7bfbdb25511cb56cc
Steve Carlson <yasteve@yahoo-inc.com>
2017-01-30 10:11:40 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by  Steve Carlson (Media Engineering) on 1/30/17.
+//  Copyright © 2017 Steve Carlson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by  Steve Carlson (Media Engineering) on 1/30/17.
+//  Copyright © 2017 Steve Carlson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
